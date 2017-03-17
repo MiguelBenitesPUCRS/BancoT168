6313a16821cf3a5e12a5114f5bd2690f0c813c39
MiguelBenites <miguelbenites@gmail.com>
2017-03-17 08:41:34 -0300

    Trabalhando no menu...

--- BancoT168/src/fila/banco/Menu.java
+++ BancoT168/src/fila/banco/Menu.java
@@ -4,7 +4,23 @@ import java.util.Scanner;
 
 public class Menu {
     
+	
+	
     public static void Menu(){
+    	
+    	Banco banco = new Banco();
+    	
+    	banco.abreCaixa(1);
+		banco.abreCaixa(2);
+		banco.abreCaixa(10);
+				
+		banco.addCliente(banco.criaCliente(70));
+		banco.addCliente(banco.criaCliente(65));
+		banco.addCliente(banco.criaCliente(67));
+		
+		banco.addCliente(banco.criaCliente(18));
+		banco.addCliente(banco.criaCliente(21));
+		banco.addCliente(banco.criaCliente(23));
     	
     	System.out.println("######                                         #    #####   #####"); 
         System.out.println("#     #   ##   #    #  ####   ####     #####  ##   #     # #     #");
@@ -13,9 +29,13 @@ public class Menu {
         System.out.println("#     # ###### #  # # #      #    #      #     #   #     # #     #") ;
         System.out.println("#     # #    # #   ## #    # #    #      #     #   #     # #     #") ;
         System.out.println("######  #    # #    #  ####   ####       #   #####  #####   #####") ;
-        System.out.println("by Miguel Benites - 2017");
-        System.out.println("------------------------------------------------------------------");
-        System.out.println("\tCadastro de clientes");
+        System.out.println("\nby Miguel Benites - 2017");
+        System.out.println("---------------------------------------------------------------------------");
+        System.out.println("*************************** Status do Banco *******************************");
+        System.out.println("[Caixas Abertos: "+banco.retornaCaixas()+"]");
+        System.out.println(banco.retornaClientes());
+        System.out.println("[Em atendimentos: ]");
+        System.out.println("******************************* Opções ************************************");
         System.out.println("0. Fim");
         System.out.println("1. Inclui");
         System.out.println("2. Altera");
